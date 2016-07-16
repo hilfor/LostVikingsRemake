6fc34a07a88c92bd0149f0c1141d58d8b9850aea
hilfor <daniel.kantorov@gmail.com>
2016-07-16 23:31:35 +0200

    Working on character changing

--- Assets/Scripts/Vikings/Characters/PlayerMotor.cs
+++ Assets/Scripts/Vikings/Characters/PlayerMotor.cs
@@ -3,36 +3,44 @@ using System.Collections;
 
 public class PlayerMotor : MonoBehaviour
 {
-    public ICharacter m_CurrentViking;
+    public ICharacter m_ActiveViking = null;
 
     void Start()
     {
-        m_CurrentViking = GetComponent<ICharacter>();
+        m_ActiveViking = GetComponent<ICharacter>();
+    }
+
+    public void SetActiveViking(ICharacter activeViking)
+    {
+        m_ActiveViking = activeViking;
     }
 
     void Move(float direction)
     {
+        if (m_ActiveViking == null)
+            return;
         float normalizedSpeed = Mathf.Abs(direction);
         if (direction < 0)
         {
-            m_CurrentViking.MoveLeft(normalizedSpeed);
+            m_ActiveViking.MoveLeft(normalizedSpeed);
         }
         else
         {
-            m_CurrentViking.MoveRight(normalizedSpeed);
+            m_ActiveViking.MoveRight(normalizedSpeed);
         }
     }
 
     public void ExecuteAction(InputState inputState)
     {
-
+        if (m_ActiveViking == null)
+            return;
         if (inputState.CheckPressed(InputAction.RIGHT) || inputState.CheckPressed(InputAction.LEFT))
         {
             Move(inputState.CheckPressed(InputAction.RIGHT) ? 1 : -1);
         }
         else
         {
-            m_CurrentViking.Action(inputState);
+            m_ActiveViking.Action(inputState);
         }
 
         //switch (state)
